2fde380615297a30ea73983bd525d50d48d9cc48
xuchendong <1062645490@qq.com>
2019-11-03 14:37:04 +0800

    测试

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,3 +1,17 @@
 <template>
-<div>我是home组件</div>
+<div>
+    <el-container>
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container><el-container>
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
+</div>
 </template>
