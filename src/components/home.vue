46b919cb56103c01b63a873eb95dabcf751fe6d2
xuchendong <1062645490@qq.com>
2019-11-03 14:40:49 +0800

    第二次测试

--- src/components/home.vue
+++ src/components/home.vue
@@ -13,5 +13,12 @@
     <el-main>Main</el-main>
   </el-container>
 </el-container>
+<el-container>
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
 </div>
 </template>
